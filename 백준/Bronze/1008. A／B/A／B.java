340c13bafd4fb970d5de10c2f2681ae45f88cefd
endoxff <162097666+endoxff@users.noreply.github.com>
2024-03-05 21:37:04 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 17984 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		System.out.println(a / b);
+	}
+
+}
