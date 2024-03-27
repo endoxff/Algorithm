255293f07779c1d472562f583f13c797a0e01fac
endoxff <162097666+endoxff@users.noreply.github.com>
2024-03-27 23:15:13 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("강한친구 대한육군").append("\n");
+		sb.append("강한친구 대한육군");
+		
+		System.out.println(sb);
+	}
+
+}
