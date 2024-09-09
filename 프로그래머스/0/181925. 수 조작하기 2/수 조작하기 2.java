45088e0c38cda6f7126b19f98919c69f2db0f95b
endoxff <162097666+endoxff@users.noreply.github.com>
2024-09-09 22:40:10 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.95 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < numLog.length - 1; i++) {
+            int n = numLog[i + 1] - numLog[i];
+            
+            switch(n) {
+                case 1:
+                    sb.append("w");
+                    break;
+                case -1:
+                    sb.append("s");
+                    break;
+                case 10:
+                    sb.append("d");
+                    break;
+                case -10:
+                    sb.append("a");
+                    break;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
